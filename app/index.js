4b232e98c2d21bb33b33ba2fca6b241b10434ed4
Rakesh Patel <emailme@rakeshrpatel.com>
2017-03-09 11:51:40 -0500

    nested components

--- app/index.js
+++ app/index.js
@@ -1,11 +1,45 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+var USER_DATA = {
+    name: 'Rakesh Patel',
+    username: 'rakeshpatel87p',
+    age: '21'
+}
 
-const HelloWorld = React.createClass({
-    render() {
-        return ( <div>Hi {this.props.name}!</div>)
-    }
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Age = React.createClass({
+  render: function () {
+	return <div>{this.props.ageUser}</div>  }
 });
 
-ReactDOM.render( < HelloWorld name="billymax"/ > , document.getElementById('app'))
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
 
+var ProfileName = React.createClass({
+  render: function (){
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <Age ageUser={this.props.user.age}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
