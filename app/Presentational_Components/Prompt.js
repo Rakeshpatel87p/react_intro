edb6b3df47d94f33ea8d2c964c505b564e097c29
Rakesh Patel <emailme@rakeshrpatel.com>
2017-04-05 14:29:39 -0400

    refractored and propTypes added

--- app/Presentational_Components/Prompt.js
+++ app/Presentational_Components/Prompt.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 
-const Prompt = React.createClass({
-	render(){
-		return (
+function Prompt(props){
+	return (
 			<div>
-    			<h1>{this.props.header}</h1>
-    		<div>
-        	<form onSubmit={this.props.onSubmitUser}>
-            <div className="formGroup">
-                <input className="formControl" 
-                	onChange={this.props.onUpdateUser} 
-                	value={this.props.username} 
-                	placeholder="GitHub Username" type="text" />
-            </div>
-            <div className="formGroup">
-                <button 
-                	className="Hey" 
-                	type="submit">
-                    Continue
-                </button>
-            </div>
-        </form>
-    </div>
-</div>)
-	}
-})
+    			<h1>{props.header}</h1>
+    			<div>
+	        		<form onSubmit={props.onSubmitUser}>
+	            	<div className="formGroup">
+	                	<input className="formControl" 
+	                		onChange={props.onUpdateUser} 
+	                		value={props.username} 
+	                		placeholder="GitHub Username" type="text" />
+	            	</div>
+	            	<div className="formGroup">
+	                	<button 
+	                		className="Hey" 
+	                		type="submit">
+	                    	Continue
+	                	</button>
+	            	</div>
+	        		</form>
+	    		</div>
+			</div>
+		)
+}
+
+
+Prompt.propTypes = {
+	header: PropTypes.string.isRequired,
+	onUpdateUser: PropTypes.func.isRequired,
+	onSubmitUser: PropTypes.func.isRequired,
+	username: PropTypes.string.isRequired
+}
 
 export default Prompt;
